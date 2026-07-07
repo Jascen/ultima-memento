18a8870963becc034e1b68749cf4e8fb45288cfa
Devin Ulrey <Devin.Ulrey@gmail.com>
2026-07-07 05:29:03 -0400

    - Wolf caps and stag caps are now sold by the fur trader

--- World/Source/Scripts/System/Misc/ChangeLog.cs
+++ World/Source/Scripts/System/Misc/ChangeLog.cs
@@ -1103,6 +1103,7 @@ namespace Server.Misc
 					builder.Append("- Item - Leggings can now be equipped over pants<br>");
 					builder.Append("- Item - Fix issue where throwing glove types had inconsistent base damage<br>");
 					builder.Append("- Item - Crafted relocking boxes now properly retain their difficulty<br>");
+					builder.Append("- Item - Wolf caps and stag caps are now sold by the fur trader<br>");
 					builder.Append("- Misc - Fix issue where Dex/Int stat gains couldn't happen if Str gain was on cooldown<br>");
 					builder.Append("- Misc - Fix issue where where some Library books would show random books<br>");
 					builder.Append("- Misc - Bandages now stop when the healer dies<br>");
