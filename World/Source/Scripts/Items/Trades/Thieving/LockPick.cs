0e98bb4e8ed520d97da15997b59fe7cda57cf920
Devin Ulrey <Devin.Ulrey@gmail.com>
2026-07-08 21:31:33 -0400

    - Add explicit IsSciFi property - Split out into CanDoEffect and DoEffect methods - Add OnComplete callback

--- World/Source/Scripts/Items/Trades/Thieving/LockPick.cs
+++ World/Source/Scripts/Items/Trades/Thieving/LockPick.cs
@@ -30,6 +30,9 @@ namespace Server.Items
 			}
 		}
 
+		[CommandProperty( AccessLevel.GameMaster )]
+		public bool IsSciFi { get { return ItemID == 0x3A75; } }
+
 		[Constructable]
 		public Lockpick() : this( 1 )
 		{
@@ -46,6 +49,7 @@ namespace Server.Items
 		public Lockpick( Serial serial ) : base( serial )
 		{
 		}
+
 		public override void Serialize( GenericWriter writer )
 		{
 			base.Serialize( writer );
@@ -69,6 +73,42 @@ namespace Server.Items
 			from.Target = new InternalTarget( this );
 		}
 
+		public static bool CanDoEffect( Mobile from, Lockpick lockpick, Item targeted, bool isLocked )
+		{
+			from.Direction = from.GetDirectionTo( targeted );
+
+			if ( targeted.Catalog == Catalogs.SciFi && isLocked && !lockpick.IsSciFi )
+			{
+				from.SendMessage( "This doesn't have a key hole, but it does have a card slot." );
+				return false;
+			}
+			else if ( targeted.Catalog == Catalogs.SciFi && isLocked && lockpick.IsSciFi )
+			{
+				from.PlaySound( 0x54B );
+				return true;
+			}
+			else if ( isLocked && !lockpick.IsSciFi )
+			{
+				from.PlaySound( 0x241 );
+				return true;
+			}
+			else if ( isLocked && lockpick.IsSciFi )
+			{
+				from.SendMessage( "You don't see a card slot on this." );
+				return false;
+			}
+			else
+			{
+				from.SendLocalizedMessage( 502069 ); // This does not appear to be locked
+				return false;
+			}
+		}
+
+		public static void DoEffect(Mobile from, Lockpick lockpick, Item targeted, Action onComplete)
+		{
+			new InternalTimer( from, (ILockpickable)targeted, lockpick, onComplete ).Start();
+		}
+
 		private class InternalTarget : Target
 		{
 			private Lockpick m_Item;
@@ -87,33 +127,11 @@ namespace Server.Items
 				{
 					UnlockUtilities.TryUnlockDoor( from, (BaseDoor)targeted, m_Item, UnlockUtilities.LockpickDoorOptions );
 				}
-				else if ( targeted is ILockpickable )
+				else if ( targeted is ILockpickable && targeted is Item )
 				{
-					Item item = (Item)targeted;
-					from.Direction = from.GetDirectionTo( item );
-
-					if ( item.Catalog == Catalogs.SciFi && ((ILockpickable)targeted).Locked && m_Item.ItemID != 0x3A75 )
+					if (CanDoEffect(from, m_Item, (Item)targeted, ((ILockpickable)targeted).Locked))
 					{
-						from.SendMessage( "This doesn't have a key hole, but it does have a card slot." );
-					}
-					else if ( item.Catalog == Catalogs.SciFi && ((ILockpickable)targeted).Locked && m_Item.ItemID == 0x3A75 )
-					{
-						from.PlaySound( 0x54B );
-						new InternalTimer( from, (ILockpickable)targeted, m_Item ).Start();
-					}
-					else if ( ((ILockpickable)targeted).Locked && m_Item.ItemID != 0x3A75 )
-					{
-						from.PlaySound( 0x241 );
-						new InternalTimer( from, (ILockpickable)targeted, m_Item ).Start();
-					}
-					else if ( ((ILockpickable)targeted).Locked && m_Item.ItemID == 0x3A75 )
-					{
-						from.SendMessage( "You don't see a card slot on this." );
-					}
-					else
-					{
-						// The door is not locked
-						from.SendLocalizedMessage( 502069 ); // This does not appear to be locked
+						DoEffect(from, m_Item, (Item)targeted, null);
 					}
 				}
 				else
@@ -121,51 +139,42 @@ namespace Server.Items
 					from.SendLocalizedMessage( 501666 ); // You can't unlock that!
 				}
 			}
+		}
 
-			private class InternalTimer : Timer
+		private class InternalTimer : Timer
+		{
+			private readonly Mobile m_From;
+			private readonly ILockpickable m_Item;
+			private readonly Lockpick m_Lockpick;
+			private readonly Action m_OnComplete;
+
+			public InternalTimer( Mobile from, ILockpickable item, Lockpick lockpick, Action onComplete ) : base( TimeSpan.FromSeconds( 3.0 ) )
 			{
-				private Mobile m_From;
-				private ILockpickable m_Item;
-				private Lockpick m_Lockpick;
+				m_From = from;
+				m_Item = item;
+				m_Lockpick = lockpick;
+				m_OnComplete = onComplete;
+				Priority = TimerPriority.TwoFiftyMS;
+			}
+
+			protected void BrokeLockPickTest()
+			{
+				// When failed, a 25% chance to break the lockpick
+				if ( Utility.Random( 4 ) == 0 )
+				{
+					// You broke the lockpick.
+					if ( m_Lockpick.IsSciFi ){ m_From.PlaySound( 0x549 ); m_From.PrivateOverheadMessage( 0, 1150, false,  "You broke the key card.", m_From.NetState ); }
+					else { m_From.PlaySound( 0x3A4 ); m_From.PrivateOverheadMessage( 0, 1150, false,  "You broke the lockpick.", m_From.NetState ); }
+
+					m_Lockpick.Consume();
+				}
+			}
 			
-				public InternalTimer( Mobile from, ILockpickable item, Lockpick lockpick ) : base( TimeSpan.FromSeconds( 3.0 ) )
-				{
-					m_From = from;
-					m_Item = item;
-					m_Lockpick = lockpick;
-					Priority = TimerPriority.TwoFiftyMS;
-				}
-
-				protected void BrokeLockPickTest()
-				{
-					// When failed, a 25% chance to break the lockpick
-					if ( Utility.Random( 4 ) == 0 )
-					{
-						Item item = (Item)m_Item;
-
-						// You broke the lockpick.
-						if ( m_Lockpick.ItemID == 0x3A75 ){ m_From.PlaySound( 0x549 ); m_From.PrivateOverheadMessage( 0, 1150, false,  "You broke the key card.", m_From.NetState ); }
-						else { m_From.PlaySound( 0x3A4 ); m_From.PrivateOverheadMessage( 0, 1150, false,  "You broke the lockpick.", m_From.NetState ); }
-
-						m_Lockpick.Consume();
-					}
-				}
-				
-				protected override void OnTick()
+			protected override void OnTick()
+			{
+				try
 				{
 					Item item = (Item)m_Item;
-
-					if ( m_From.Skills[SkillName.Lockpicking].Base < 1 )
-					{
-						int cycle = 10;
-
-						while ( cycle > 0 )
-						{
-							cycle--;
-							m_From.CheckTargetSkill( SkillName.Lockpicking, m_Item, 0, 10 );
-						}
-					}
-
 					if ( !m_From.InRange( item.GetWorldLocation(), 2 ) )
 						return;
 
@@ -173,7 +182,7 @@ namespace Server.Items
 					{
 						// LockLevel of 0 means that the door can't be picklocked
 						// LockLevel of -255 means it's magic locked
-						if ( m_Lockpick.ItemID == 0x3A75 ){ m_From.PrivateOverheadMessage( 0, 1150, false,  "This lock cannot be hacked by normal means.", m_From.NetState ); }
+						if ( m_Lockpick.IsSciFi ){ m_From.PrivateOverheadMessage( 0, 1150, false,  "This lock cannot be hacked by normal means.", m_From.NetState ); }
 						else { m_From.PrivateOverheadMessage( 0, 1150, false,  "This lock cannot be picked by normal means.", m_From.NetState ); }
 
 						return;
@@ -186,7 +195,7 @@ namespace Server.Items
 						m_From.CheckSkill( SkillName.Lockpicking, 0, m_Item.LockLevel );*/
 
 						// The LockLevel is higher thant the LockPicking of the player
-						if ( m_Lockpick.ItemID == 0x3A75 ){ m_From.PrivateOverheadMessage( 0, 1150, false,  "You don't see how that lock can be hacked.", m_From.NetState ); }
+						if ( m_Lockpick.IsSciFi ){ m_From.PrivateOverheadMessage( 0, 1150, false,  "You don't see how that lock can be hacked.", m_From.NetState ); }
 						else { m_From.PrivateOverheadMessage( 0, 1150, false,  "You don't see how that lock can be manipulated.", m_From.NetState ); }
 						return;
 					}
@@ -194,7 +203,7 @@ namespace Server.Items
 					if ( m_From.CheckTargetSkill( SkillName.Lockpicking, m_Item, m_Item.LockLevel, m_Item.MaxLockLevel ) )
 					{
 						// Success! Pick the lock!
-						if ( m_Lockpick.ItemID == 0x3A75 ){ m_From.PlaySound( 0x54B ); m_From.PrivateOverheadMessage( 0, 1150, false,  "Your skill at hacking worked.", m_From.NetState ); }
+						if ( m_Lockpick.IsSciFi ){ m_From.PlaySound( 0x54B ); m_From.PrivateOverheadMessage( 0, 1150, false,  "Your skill at hacking worked.", m_From.NetState ); }
 						else { m_From.PlaySound( 0x4A ); m_From.PrivateOverheadMessage( 0, 1150, false,  "The lock quickly yields to your skill.", m_From.NetState ); }
 						
 						m_Item.LockPick( m_From );
@@ -204,68 +213,73 @@ namespace Server.Items
 						// The player failed to pick the lock
 						BrokeLockPickTest();
 
-						if ( m_Lockpick.ItemID == 0x3A75 ){ m_From.PrivateOverheadMessage( 0, 1150, false,  "You are unable to hack the lock.", m_From.NetState ); }
+						if ( m_Lockpick.IsSciFi ){ m_From.PrivateOverheadMessage( 0, 1150, false,  "You are unable to hack the lock.", m_From.NetState ); }
 						else { m_From.PrivateOverheadMessage( 0, 1150, false,  "You are unable to pick the lock.", m_From.NetState ); }
 
-                        // ==== Random Item Disintergration upon Failure ====
-                        if (m_Item is TreasureMapChest)
-                        {
-                            int i_Num = 0; Item i_Destroy = null;
+						// ==== Random Item Disintergration upon Failure ====
+						if (m_Item is TreasureMapChest)
+						{
+							int i_Num = 0; Item i_Destroy = null;
 
-                            BaseContainer m_chest = m_Item as BaseContainer;                            
-                            Item Dust = new DustPile();
-                            
-                            for (int i = 10; i > 0; i--)
-                            {
-                                i_Num = Utility.Random(m_chest.Items.Count);
-                                // Make sure DustPiles aren't called for destruction
-                                if ((m_chest.Items.Count > 0) && m_chest.Items[i_Num] is DustPile)
-                                {
-                                    for (int ci = (m_chest.Items.Count - 1); ci >= 0; ci--)
-                                    {
-                                        i_Num = ci;
-                                        if (i_Num < 0) { i_Num = 0; }
+							BaseContainer m_chest = m_Item as BaseContainer;
+							Item Dust = new DustPile();
+							
+							for (int i = 10; i > 0; i--)
+							{
+								i_Num = Utility.Random(m_chest.Items.Count);
+								// Make sure DustPiles aren't called for destruction
+								if ((m_chest.Items.Count > 0) && m_chest.Items[i_Num] is DustPile)
+								{
+									for (int ci = (m_chest.Items.Count - 1); ci >= 0; ci--)
+									{
+										i_Num = ci;
+										if (i_Num < 0) { i_Num = 0; }
 
-                                        if (m_chest.Items[i_Num] is DustPile)
-                                        {
-                                            i_Destroy = null;
-                                        }
-                                        else
-                                        {
-                                            i_Destroy = m_chest.Items[i_Num];
-                                            i = 0;
-                                        }
-                                        // Nothing left but Dust
-                                        if (ci < 0 && i > 0)
-                                        {
-                                            i_Destroy = null; i = 0;
-                                        }
-                                    }
-                                }
-                                // Item targeted =+= prepare for object DOOM! >;D
-                                else
-                                {
-                                    i_Destroy = m_chest.Items[i_Num]; i = 0;
-                                }
-                            }                            
-                            // Delete chosen Item and drop a Dust Pile
-                            if (i_Destroy is Gold)
-                            {
-                                if (i_Destroy.Amount > 1000)
-                                    i_Destroy.Amount -= 1000;
-                                else
-                                    i_Destroy.Delete();
+										if (m_chest.Items[i_Num] is DustPile)
+										{
+											i_Destroy = null;
+										}
+										else
+										{
+											i_Destroy = m_chest.Items[i_Num];
+											i = 0;
+										}
+										// Nothing left but Dust
+										if (ci < 0 && i > 0)
+										{
+											i_Destroy = null; i = 0;
+										}
+									}
+								}
+								// Item targeted =+= prepare for object DOOM! >;D
+								else
+								{
+									i_Destroy = m_chest.Items[i_Num]; i = 0;
+								}
+							}
+							// Delete chosen Item and drop a Dust Pile
+							if (i_Destroy is Gold)
+							{
+								if (i_Destroy.Amount > 1000)
+									i_Destroy.Amount -= 1000;
+								else
+									i_Destroy.Delete();
 
-                                Dust.Hue = 1177; m_chest.DropItem(Dust);
-                            }
-                            else if (i_Destroy != null)
-                            {
-                                i_Destroy.Delete(); m_chest.DropItem(Dust);
-                            }
-                            Effects.PlaySound(m_chest.Location, m_chest.Map, 0x1DE);
-                            m_chest.PublicOverheadMessage(MessageType.Regular, 2004, false, "The sound of gas escaping is heard from the chest.");
-                        }
+								Dust.Hue = 1177; m_chest.DropItem(Dust);
+							}
+							else if (i_Destroy != null)
+							{
+								i_Destroy.Delete(); m_chest.DropItem(Dust);
+							}
+							Effects.PlaySound(m_chest.Location, m_chest.Map, 0x1DE);
+							m_chest.PublicOverheadMessage(MessageType.Regular, 2004, false, "The sound of gas escaping is heard from the chest.");
+						}
 					}
+				}
+				finally
+				{
+					if (m_OnComplete != null)
+						m_OnComplete.Invoke();
 				}
 			}
 		}
